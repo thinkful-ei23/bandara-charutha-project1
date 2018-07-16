eaa7122349310f3a27905458bf800cb6bbd9e5df
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-07-16 14:42:02 -0400

    question-5 no error

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,9 +1,11 @@
 'use strict'
-
-function whoAmI(name, age) {
-  console.log("Hi, my name is " + name + " and I'm " + age + " years old");
+function yearOfBirth(age) {
   let yearOfBirth = 2018 - age;
   console.log("I was born in " + yearOfBirth);
 }
 
-console.log(whoAmI("charutha", 22));
+function whoAmI(name, age) {
+  console.log("Hi, my name is " + name + " and I'm " + age + " years old");
+  let yob = yearOfBirth(age);
+}
+whoAmI('Charutha', 22);
