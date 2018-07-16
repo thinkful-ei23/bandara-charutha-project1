1f8dd6a6952e6cc764f1b2dd6d9751b50142c7b7
Charutha Bandara <charuthabandara@gmail.com>
2018-07-16 14:33:58 -0400

    yearOfBirth

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,5 +1,9 @@
 'use strict'
 
 function whoAmI(name, age) {
-  console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+  console.log("Hi, my name is " + name + " and I'm " + age + " years old");
+  let yearOfBirth = 2018 - age;
+  console.log("I was born in " + yearOfBirth);
 }
+
+console.log(whoAmI("charutha", 22));
