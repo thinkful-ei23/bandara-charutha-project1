7dcc3d298764f537922c7cf576646c8abf11859e
john <johnjacobcosgrove@gmail.com>
2018-07-18 14:02:16 -0400

    added loaf object

---
+++ july/18/advanceddrills.js
@@ -0,0 +1,10 @@
+const loaf = {
+    flour: 300,
+    water: 210,
+    hydration:function(){
+        return loaf.water / (loaf.flour *100)
+    }
+}
+console.log( loaf.flour, loaf.water)
+
+console.log(loaf.hydration())
