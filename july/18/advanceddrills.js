1b9d9449a7c333806554160afd77b99383d30615
john <johnjacobcosgrove@gmail.com>
2018-07-18 16:44:54 -0400

    factory?

--- july/18/advanceddrills.js
+++ july/18/advanceddrills.js
@@ -96,3 +96,44 @@ console.log(decode(message));
 
 
 
+function createCharacter(name,nickname, race, origin, attack, defense, weapon){
+
+return {
+    name, nickname, race, origin, attack, defense, weapon,
+    describe(){
+        console.log(`${this.name} is a ${this.race} from ${this.origin} who uses a ${weapon}`)
+    },
+    evauateFight (character){
+        let dealDamage = this.attack - character.defense
+        let takeDamage = character.attack - this.defense
+
+        if (dealDamage < 0){
+            dealDamage = 0
+        }
+        if (takeDamage < 0){
+            takeDamage =0
+        }
+        console.log(`Your opponent takes ${dealDamage} and you receive ${takeDamage} damage`)
+    }
+}
+
+}
+
+const character = [
+    createCharacter('gandalf the white','gandalf','Wizard','Middle Earth',10,6,'staff'),
+    createCharacter('bilbo baggins','bilbo', 'hobbit', 'the shire',2,1,'ring'),
+    createCharacter('frodo baggins','frodo','hobbit','the shire',3,2,'string and barrow blade'),
+    createCharacter('aragorn son of arathorn','aragorn','man','dunnedain',6,8,'anduril'),
+    createCharacter('legolas','legolas','elf','woodland realm',8,5,'bow and arrow'),
+]
+
+character.push(createCharacter('arwen undomiel','arwen', 'half-elf','rivendell',75,2,'hadhafang'))
+
+character.find(person => person.nickname === 'aragorn').describe
+
+const hobbitArray = character.filter(person => person.race === 'hobbit')
+console.log(hobbitArray)
+
+const highAttk = character.filter(person => person.attack > 5)
+console.log(highAttk)
+
