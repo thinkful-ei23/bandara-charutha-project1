cb01ecd61a22b7ae5d5291fa833b270a1e4be85f
Charutha Bandara <charuthabandara@gmail.com>
2018-07-18 14:23:58 -0400

    drill 2

--- july/18/advanceddrills.js
+++ july/18/advanceddrills.js
@@ -8,3 +8,22 @@ const loaf = {
 console.log( loaf.flour, loaf.water)
 
 console.log(loaf.hydration())
+
+// 2
+const object1 = {
+    foo: 'foo',
+    bar: 'bar',
+    fum: 'fum',
+    quux: 'quux',
+    spam: 'spam'
+}
+
+function loopObject1(data) {
+    for (let word in data) {
+        const value = data[word];
+        console.log(word+ ": " + value);
+    } 
+}
+
+loopObject1(object1) 
+// console.log(Object.keys(Object1));
