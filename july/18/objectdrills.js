d5de71e1483202d4d5e16a7569b3bac25fe05d32
Charutha Bandara <charuthabandara@gmail.com>
2018-07-18 11:48:44 -0400

    number 1

---
+++ july/18/objectdrills.js
@@ -0,0 +1,12 @@
+function createMyObject() {
+    return {
+        foo: 'bar',
+        answerToUniverse: 42,
+        'olly olly': 'oxen free',
+        sayHello: function() {
+            return 'hello';
+        }
+    };
+}
+
+console.log(createMyObject());
