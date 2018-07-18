c1bfb8e941854c5108c330d9eab8cb710c812af2
john <johnjacobcosgrove@gmail.com>
2018-07-18 11:57:48 -0400

    added update function

--- july/18/objectdrills.js
+++ july/18/objectdrills.js
@@ -10,3 +10,12 @@ function createMyObject() {
 }
 
 console.log(createMyObject());
+
+function updateObject(obj){
+    obj.foo = 'foo';
+    obj.bar = 'bar';
+    obj.bizz = 'bizz';
+    obj.bang = 'bang';
+    
+    return obj
+}
