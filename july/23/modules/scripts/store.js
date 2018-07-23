adecb4f3e9a174dbcb55f94449f3cd1be9f7a653
Shelly Leung <pontificable@yahoo.com>
2018-07-23 13:36:20 -0700

    more modules and methods

--- july/23/modules/scripts/store.js
+++ july/23/modules/scripts/store.js
@@ -1,4 +1,4 @@
-/* global shoppingList, cuid, $ */
+/* global shoppingList, cuid, $, Items */
 // eslint-disable-next-line no-unused-vars
 
 const store = (function () {
@@ -11,7 +11,26 @@ const store = (function () {
 	];
 	let hideCheckedItems = false;
 	let searchTerm = '';
+    
+	const findById = function(id) {
+		return store.items.find(item => item.id === id);
+	};
 
-	return {items, hideCheckedItems, searchTerm};
+	const addItem = function(name) {
+		try {
+			Items.validateName(name);
+			Items.create(name);
+			this.items(items.create(name))
+		} catch(error) {
+			console.log('Cannot add items:' + error.message);
+		}
+	};
+
+	const findAndToggleChecked = function(id) {
+		const foundItem = this.findById(id);
+		foundItem.checked = !foundItem.checked;
+
+	};
+
+	return {items, hideCheckedItems, searchTerm, findById, addItem, findAndToggleChecked};
 }() );
-
